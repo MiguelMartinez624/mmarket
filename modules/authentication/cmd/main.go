ba3ea052f11637572e138e1f7da363ca52eacb1e
miguelmartinez624 <miguelesalador@gmail.com>
2020-08-18 21:30:12 -0400

    main for auth module

--- modules/authentication/cmd/main.go
+++ modules/authentication/cmd/main.go
@@ -1,8 +1,57 @@
-package cmd
+package main
 
-import auth "github.com/miguelmartinez624/mmarket/modules/authentication"
+import (
+	"context"
+	"fmt"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	auth "github.com/miguelmartinez624/mmarket/modules/authentication"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+	"time"
+)
+
+const DB_URI = "mongodb://localhost:27017"
 
 func main() {
+	client, cancel := ConnectMongoDB(DB_URI)
+	defer cancel()
+	r := mux.NewRouter()
+	authModule := auth.BuildAuthModule(client, r)
 
-	authModule := auth.BuildAuthModule()
+	// Service start
+	handler := handlers.CORS(
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT"}),
+		handlers.AllowedHeaders([]string{"Accept", "Accept-Language", "Content-Type", "Content-Language", "Origin"}),
+		handlers.AllowedOrigins([]string{"*"}),
+	)(r)
+	// corsObj := handlers.AllowedOrigins([]string{"*"})
+	srv := &http.Server{
+		Handler: handler,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	fmt.Println("Up and running")
+	log.Fatal(srv.ListenAndServe())
+}
+
+
+func ConnectMongoDB(uri string) (client *mongo.Client, cancel context.CancelFunc) {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client, cancel
 }
